5afcd1aa35a70fdd8058c14672d598147ffe6524
Almatin 22 <rolandmaulana8@gmail.com>
2025-06-11 08:12:57 +0700

    revert profile

--- src/main/java/com/bni/bni/service/ProfileService.java
+++ src/main/java/com/bni/bni/service/ProfileService.java
@@ -1,33 +1,33 @@
-package com.bni.bni.service;
+// package com.bni.bni.service;
 
-import java.time.LocalDate;
-import java.time.OffsetDateTime;
+// import java.time.LocalDate;
+// import java.time.OffsetDateTime;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import com.bni.bni.entity.Profile;
-import com.bni.bni.repository.ProfileRepository;
+// import com.bni.bni.entity.Profile;
+// import com.bni.bni.repository.ProfileRepository;
 
-@Service
-public class ProfileService {
+// @Service
+// public class ProfileService {
     
-    @Autowired
-    private ProfileRepository repo_profile;
+//     @Autowired
+//     private ProfileRepository repo_profile;
 
-    public String addProfile(Long user_id, String first_name, String last_name, String place_of_birth, LocalDate date_of_birth) {
-        Profile profile = new Profile();
-        profile.setUser_id(user_id);
-        profile.setFirst_name(first_name);
-        profile.setLast_name(last_name);
-        profile.setPlace_of_birth(place_of_birth);
-        profile.setDate_of_birth(date_of_birth);
-        profile.setCreatedAt(OffsetDateTime.now());
-        profile.setUpdatedAt(OffsetDateTime.now());
-        repo_profile.save(profile);
+//     public String addProfile(Long user_id, String first_name, String last_name, String place_of_birth, LocalDate date_of_birth) {
+//         Profile profile = new Profile();
+//         profile.setUser_id(user_id);
+//         profile.setFirst_name(first_name);
+//         profile.setLast_name(last_name);
+//         profile.setPlace_of_birth(place_of_birth);
+//         profile.setDate_of_birth(date_of_birth);
+//         profile.setCreatedAt(OffsetDateTime.now());
+//         profile.setUpdatedAt(OffsetDateTime.now());
+//         repo_profile.save(profile);
 
-        return "Profile added successfully";
-    }
+//         return "Profile added successfully";
+//     }
 
     // public String showProfiles(Long user_id) {
     //     Optional<Profile> profile = repo_profile.findByUser_id(user_id);
@@ -37,4 +37,4 @@ public class ProfileService {
 
     //     return null; 
     // } 
-}
+// }
